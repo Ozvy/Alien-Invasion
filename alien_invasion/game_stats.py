5c39c3f6e2a8454a1aba36d4820b8bf6e3ca92e6
Ozvy <ewhittington@student.cscc.edu>
2025-04-18 13:36:52 -0400

    fleshed out game states to be updated by game

--- alien_invasion/game_stats.py
+++ alien_invasion/game_stats.py
@@ -1,5 +1,35 @@
+import pygame
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from alien_invasion import AlienInvasion
+
+
 class GameStats():
 
-    def __init__(self, ship_limit):
-        self.ships_left = ship_limit
+    def __init__(self, game: 'AlienInvasion'):
+        self.game = game
+        self.settings = game.settings
+        self.max_score = 0
+        self.reset_stats()
+
+    def reset_stats(self):
+        self.ships_left = self.settings.starting_ship_count
+        self.score = 0
+        self.level= 1
+
+    def update(self, collisions):
+        self._update_score(collisions)
+
+    def _update_score(self, collisions):
+        for alien in collisions:
+            self.score += self.settings.alien_points
+    def _update_max_score(self):
+        if self.score > self.max_score:
+            self.max_score = self.score
+    def update_level(self):
+        self.level += 1
+
+
+
         
